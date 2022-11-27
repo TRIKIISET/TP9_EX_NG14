1bf9c86377e7da6f42296d287f2d01e16a7d2618
Amel TRIKI <amel.triki@gmail.com>
2022-11-27 20:11:43 +0100

    TP9 EX fini

--- src/app/EXERCICE/employe/employe.component.ts
+++ src/app/EXERCICE/employe/employe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Employe } from '../model/employe';
+import { EmployeService } from '../services/employe.service';
 
 @Component({
   selector: 'app-employe',
@@ -7,9 +10,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeComponent implements OnInit {
 
-  constructor() { }
+  lesEmployes!:Employe[];
+  employeForm!:FormGroup;
+  constructor(private employeService:EmployeService,
+    private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.employeService.getEmployes().subscribe(
+      data => this.lesEmployes = data
+    )
+    this.createForm();
+  }
+
+  createForm(){
+    this.employeForm = this.fb.nonNullable.group({
+      matricule:[0,[Validators.required,Validators.pattern('^[1-9]{3}[0-9]*$')]],
+      nom:['',[Validators.required, Validators.pattern('^[A-Z][a-z]+( [A-Z][a-z]+)+$')]],
+      dateEmbauche: ["2022-11-27"],
+      diplomes:this.fb.array([])
+    })
+  }
+
+  get matricule(){
+    return this.employeForm.get('matricule');
+  }
+
+  get nom(){
+    return this.employeForm.get('nom');
+  }
+
+  get lesDiplomes(){
+    return this.employeForm.get('diplomes') as FormArray;
+  }
+
+  ajouterDiplome(){
+    this.lesDiplomes.push(this.fb.group({
+      nomDiplome:['',[Validators.required, Validators.minLength(3)] ],
+      annee:[2022, [Validators.min(1960), Validators.max(2022)]]
+    }))
+  }
+
+  // get nomDiplome(i:number){
+  //   return 
+  // }
+
+  isDiplomNameValid(i:number){
+      let dip = this.lesDiplomes.controls[i].get('nomDiplome');
+      return dip?.invalid && dip?.touched;
+  }
+
+  isYearValid(i:number){
+    let an =  this.lesDiplomes.controls[i].get('annee');
+    return an?.invalid && an?.touched;
+  }
+
+  ajouterEmploye(){
+    this.employeService.addEmploye(this.employeForm.value).subscribe(
+      employe=> this.lesEmployes.push(employe)
+    )
+  }
+
+  onVider(){
+    this.employeForm.reset();
+    this.lesDiplomes.clear();
   }
 
 }
--- src/app/EXERCICE/services/employe.service.ts
+++ src/app/EXERCICE/services/employe.service.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employe } from '../model/employe';
 
+const URL ="http://localhost:3000/employes";
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getEmployes():Observable<Employe[]>{
+    return this.http.get<Employe[]>(URL);
+  }
+
+  addEmploye(emp:Employe):Observable<Employe>{
+    return this.http.post<Employe>(URL, emp);
+  }
 
 }
